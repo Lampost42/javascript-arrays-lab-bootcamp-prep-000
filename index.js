ceba95e80c2101a313a59c1a5ca706028230add5
Trevor Russell <truss.42@gmail.com>
2018-05-17 16:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function destructivelyRemoveFirstKitten(){
   return(kittens)
 }
 function appendKitten(name) {
-  kitten = [...kitten, name]
-  return kitten
+  kittens = [...kitten, name]
+  return kittens
 }
